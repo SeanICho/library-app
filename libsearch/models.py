d56cf1538a849d43e1c79ac53e34f79702bbfa58
Sean Cho <ihcho131313@gmail.com>
2019-07-10 12:27:29 -0500

    data

--- libsearch/models.py
+++ libsearch/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    change_message = models.CharField(max_length=10, blank=True)
+    title = models.CharField(max_length=100)
+    location = models.CharField(max_length=30)
+    time = models.DateField()
+    group = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    description = models.CharField(max_length=300)
+    register = models.BooleanField()
+
+    def __str__(self):
+        return title
