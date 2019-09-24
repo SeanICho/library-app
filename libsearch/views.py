6351a0feda573392bba8e9e7c28370e23422dc0c
Sean Cho <ihcho131313@gmail.com>
2019-09-24 03:27:35 -0500

    new view in progress 2

--- libsearch/views.py
+++ libsearch/views.py
@@ -4,17 +4,7 @@ from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import render
 
-from selenium import webdriver
-import os
-from datetime import datetime
-import time as t
-from bs4 import BeautifulSoup
-import pandas as pd
-import json
-
-import requests
-requests.packages.urllib3.disable_warnings()
-from bs4 import BeautifulSoup
+import
 
 # class EventListView(ListView):
 #     model = EventDetails
@@ -23,30 +13,7 @@ from bs4 import BeautifulSoup
 #         events = model.objects.all()
 #         return render(request, 'home.html', {'events': events})
 
-def scrape(url, **class):
-    session = requests.Session()
-    session.headers = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
 
-    # url_mppl =
-    # url_dppl =
-    content = session.get(url, verify=False).content
-
-    soup = BeautifulSoup(content.encode("utf-8"), "html.parser")
-
-    event_data = soup.find_all(class_=class['event_data'])
-
-    year_today = datetime.now().year
-
-    event_change_message = ['' if event.find(class_=class['event_change_message']") == None else event.find(class_=class['event_change_message']).get_text() for event in event_data]
-    event_title = [event.find(class_=class["event_title"]).get_text() for event in event_data]
-    event_location = [event.find(class_=class['event_location']).get_text() for event in event_data]
-    event_time = [event.find(class_=class['event_time']).get_text() for event in event_data]
-    event_group = [event.find(class_=class['event_group']).get_text() for event in event_data]
-    event_type = [event.find(class_=class['event_type']).get_text() for event in event_data]
-    event_descr = [event.find(class_=class['event_descr']).get_text() for event in event_data]
-    event_register = [True if event.find(class_=class['event_register']) != None else False  for event in event_data]
-
-    
 nmdl_class = {
     'event_data': "eelistevent-data"
     'event_change_message': "eelist-changed-message"
